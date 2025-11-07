acfd1f0debb0563ccff70de5d4dc5bf567bdb672
7cem <kutaycemacar@gmail.com>
2025-11-07 07:11:01 -0800

    Initial Flask + PostgreSQL + Docker project

---
+++ app/routes.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, request, jsonify
+from app import db
+from app.models import Book
+
+main = Blueprint('main', __name__)
+
+@main.route("/books", methods=["GET"])
+def get_books():
+    books = Book.query.all()
+    return jsonify([b.to_dict() for b in books])
+
+@main.route("/books", methods=["POST"])
+def add_book():
+    data = request.json
+    book = Book(title=data["title"], author=data["author"])
+    db.session.add(book)
+    db.session.commit()
+    return jsonify(book.to_dict()), 201
+
+@main.route("/books/<int:id>", methods=["PUT"])
+def update_book(id):
+    book = Book.query.get_or_404(id)
+    data = request.json
+    book.title = data.get("title", book.title)
+    book.author = data.get("author", book.author)
+    db.session.commit()
+    return jsonify(book.to_dict())
+
+@main.route("/books/<int:id>", methods=["DELETE"])
+def delete_book(id):
+    book = Book.query.get_or_404(id)
+    db.session.delete(book)
+    db.session.commit()
+    return '', 204
